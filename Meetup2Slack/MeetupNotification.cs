4808584a7ef8aef6a4140ebd4736e64a7455d396
Jason Holloway <miceeatnicerice@hotmail.com>
2015-10-28 17:29:28 +0000

    Initial commit; mostly in place

---
+++ Meetup2Slack/MeetupNotification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetup2Slack
+{
+    public class MeetupNotification
+    {
+        public string link { get; set; }
+        public long id { get; set; }
+        public Target target { get; set; }
+        public string text { get; set; }
+        public string kind { get; set; }
+    }
+
+    public class Target
+    {
+        public string type { get; set; }
+        public string event_id { get; set; }
+        public int group_id { get; set; }
+        public string group_urlname { get; set; }
+        public int? comment_id { get; set; }
+        public bool? pending { get; set; }
+    }
+
+}
---
+++ Meetup2Slack/Program.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Meetup2Slack
+{
+    class Program
+    {
+        static Uri _redisUrl = new Uri(ConfigurationManager.AppSettings["REDISTOGO_URL"]);
+        static string _meetupNewsUrl = ConfigurationManager.AppSettings["MEETUP_NOTIFICATION_URL"];
+        static string _slackWebhookUrl = ConfigurationManager.AppSettings["SLACK_WEBHOOK_URL"];
+
+        static int _meetupGroupId = 18916580;
+        static string _lastNotificationIDKey = "LastMeetupNotificationID";
+        
+        static HttpClient _http = new HttpClient();
+
+        static void Main(string[] args) 
+        {
+            while(true) 
+            {
+                using(var redis = new RedisClient(_redisUrl)) 
+                {                        
+                    var lastNotificationID = redis.Get<long>(_lastNotificationIDKey);
+                    
+                    var news = GetMeetupNotifications();
+                                                
+                    var freshNews = news.Where(n => n.id > lastNotificationID);
+                        
+                    if(freshNews.Any()) 
+                    {
+                        var relevantNews = freshNews.Where(n => n.target.group_id == _meetupGroupId);
+                        
+                        foreach(var item in relevantNews) { 
+                            PostNotificationToSlack(item);
+                        }
+                               
+                        redis.Set(_lastNotificationIDKey, news.Max(n => n.id));
+                    }                     
+                }
+
+                Thread.Sleep(60000);
+            }
+
+        }
+
+
+
+        static IEnumerable<MeetupNotification> GetMeetupNotifications() 
+        {
+            var httpResult = _http.GetAsync(_meetupNewsUrl).Result.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<List<MeetupNotification>>(httpResult);
+        }
+
+
+        static void PostNotificationToSlack(MeetupNotification meetupNews) 
+        {
+            var content = new StringContent(
+                                JsonConvert.SerializeObject(new { text = PrepareSlackMessage(meetupNews) }),
+                                Encoding.UTF8,
+                                "application/json");
+
+            _http.PostAsync(_slackWebhookUrl, content).Wait();
+        }
+
+
+
+              
+
+
+        static string PrepareSlackMessage(MeetupNotification meetupNews) {
+            var desc = Regex.Replace(meetupNews.text, @"<b>(.*?)<\/b>", @"*$1*");
+
+            return $"{desc}\n[<{meetupNews.link}|link>]";
+
+
+        }
+
+
+
+    }
+}
